7127ce314a12f02dc209c6546d306d3a50760be7
haleema-shah <haleemasha415@gmail.com>
2024-07-10 08:06:04 -0700

    Add files via upload

---
+++ Haleema 04/Revers.py
@@ -0,0 +1,8 @@
+# given string #
+sentence=("Python is fun")
+words=sentence.split()
+# Reversed the list of words #
+reversed_words=words[::-1]
+print(reversed_words)
+output=' '.join(reversed_words)
+print(output)
